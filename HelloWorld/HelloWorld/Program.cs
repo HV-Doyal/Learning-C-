cfe5cda389facb206dfeb5f32f9745fb85d291b5
Harshvardhan Vir Doyal <128219288+HV-Doyal@users.noreply.github.com>
2024-04-16 01:36:21 +0400

    Clear Code

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,16 +4,7 @@
     {
         static void Main(string[] args)
         {
-            int number,num2;
-            float pi = 3.1415f;
-            bool isreal = true;
-            string myName = "Denis";
-            char at = '@';
-
-            number = 13;
-            num2 = 11;
-            Console.WriteLine("HelloWorld!" + number + pi + isreal + myName + at);
-            Console.ReadLine();
+            
         }
     }
 }
